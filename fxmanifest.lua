d72f93961014d811bf8b8d6400b423552c2f6f57
Minik Militan <138879599+militancc@users.noreply.github.com>
2024-06-06 21:45:27 +0300

    minik-illegals
    minik-illegals

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Minik-Illegals - Illegal activities script for FiveM based on qb-core'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua',
+    'client/config.lua'
+}
+
+server_scripts {
+    'server/main.lua',
+    'server/config.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
